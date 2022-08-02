2d516fca0f3258a9525c15e941334d832be9a30f
brochut <basile.rochut@gmail.com>
2022-08-02 14:57:17 +0200

    Add arrondi 10 aine divers

--- mwi_tools/divers.py
+++ mwi_tools/divers.py
@@ -1,5 +1,6 @@
 """Diverse functions usefull for development purposes
 """
+from math import *
 
 def x_round_25(x:float) -> float:
     """Round to the nearest 0.25 value
@@ -24,3 +25,9 @@ def x_round_50(x:float) -> float:
     """
 
     return round(x*2)/2
+
+def round_10_sup(x:float) -> int: 
+    return ceil(x/10)*10
+
+def round_10_inf(x:float) -> int: 
+    return floor(x/10)*10
