6d7adbecf090c3e18427457f802a29d06dee6e46
brochut <basile.rochut@gmail.com>
2022-08-29 14:23:44 +0200

    change polar plot

--- mwi_tools/polaire/polaire.py
+++ mwi_tools/polaire/polaire.py
@@ -73,6 +73,6 @@ def plot_polaire_and_cloud(df, df_cloud, speed, symetrique=False, nom=None) :
     set_ax_plot_polaire(df, ax, speed, nom=nom)
     df_cloud_speed = df_cloud.loc[(df_cloud['TWS'] >= speed-0.5) & (df_cloud['TWS'] <= speed+0.5)].copy()
     if symetrique : 
-        df_cloud_speed.loc['TWA'] = df_cloud_speed.loc[df_cloud['TWA'] < 0, ['TWA']].apply(lambda x : -x)
+        df_cloud_speed.loc[df_cloud['TWA'] < 0, ['TWA']] = df_cloud_speed.loc[df_cloud['TWA'] < 0, ['TWA']].apply(lambda x : -x)
     ax.plot(df_cloud_speed.TWA*np.pi/180, df_cloud_speed.speed, 'o', 'b')
     plt.show()
